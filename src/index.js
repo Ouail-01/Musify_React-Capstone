64720f698b76de298ed9be96f48170168c653027
Ouail-01 <ouaillaarabi@gmail.com>
2023-05-25 09:21:48 +0100

    Update the index file

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/configureStore';
+import Song from './components/Music/Song';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
+      <Song />
     </Provider>
   </React.StrictMode>,
 );
