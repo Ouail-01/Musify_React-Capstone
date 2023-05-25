b5178e9f643f42982dd2361fda694b6e1cf78a32
Ouail-01 <ouaillaarabi@gmail.com>
2023-05-25 10:09:39 +0100

    Fix the linters

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-/* eslint-disable */
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Home/Header';
-import "./styles/index.css";
-import Home from "./routes/Home";
-import Music from "./routes/Music";
-import Search from "./routes/Search";
-
+import './styles/index.css';
+import Home from './routes/Home';
+import Music from './routes/Music';
+import Search from './routes/Search';
 
 const App = () => (
   <div className="App">
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />,
-        <Route path="/music/:id" element={<Music />} />,
-        <Route path="searchResults/:id" element={<Search />} />,
-        <Route path="*" element={<Home />} />,
+        <Route path="/" element={<Home />} />
+        ,
+        <Route path="/music/:id" element={<Music />} />
+        ,
+        <Route path="searchResults/:id" element={<Search />} />
+        ,
+        <Route path="*" element={<Home />} />
+        ,
       </Routes>
     </Router>
   </div>
--- src/components/Home/SearchResults.js
+++ src/components/Home/SearchResults.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
