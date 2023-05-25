46dee57772dcfb7c8415558cae4c90764a237ae5
Ouail-01 <ouaillaarabi@gmail.com>
2023-05-25 09:18:52 +0100

    Add the header

---
+++ src/components/Home/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaAngleLeft, FaStar } from 'react-icons/fa';
+import SearchEngine from './SearchEngine';
+
+const Header = () => (
+  <header>
+    <ul>
+      <li>
+        <Link to="/">
+          <FaAngleLeft className="icon" />
+        </Link>
+      </li>
+      <li>
+        <h4>Music list</h4>
+      </li>
+      <li>
+        <Link to="/favorites">
+          <FaStar className="icon" />
+        </Link>
+        <SearchEngine />
+      </li>
+    </ul>
+  </header>
+);
+export default Header;
