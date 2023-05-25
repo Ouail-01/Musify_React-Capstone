445b3ba8629e12cdc79b30fb1e0ea499567d7491
Ouail-01 <ouaillaarabi@gmail.com>
2023-05-25 09:42:48 +0100

    Add the backgroung image

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Home/Header';
-import "./index.css";
+import "./styles/index.css";
 import Home from "./routes/Home";
 import Music from "./routes/Music";
 import Search from "./routes/Search";
--- src/components/Home/Header.js
+++ src/components/Home/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FaAngleLeft, FaStar } from 'react-icons/fa';
 import SearchEngine from './SearchEngine';
+import '../../styles/Header.css';
 
 const Header = () => (
   <header>
